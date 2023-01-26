cec2ed0fc3ffff8b3ced0225daf0ab4dd75bf2a3
Gerardo Gomez <gerardo.gomez@kiubix.com>
2023-01-26 09:59:16 -0600

    Items a and b of exercise 2 completed

--- algoritmo_genetico_v2.py
+++ algoritmo_genetico_v2.py
@@ -19,6 +19,26 @@ def crear_cadena(poblacion):
         poblacion_cadena.append(''.join(cromosoma))
     return poblacion_cadena
 
+def ordenar_poblacion(poblacion_strings):
+    """Funcion que ordena la poblacion de mayor a menor
+    conforme al numero de ceros que contenga cada cromosoma"""
+    poblacion_ordenada = []
+    poblacion_ordenada = sorted(poblacion_strings, key=lambda x: x.count('0'), reverse=True)
+    return poblacion_ordenada
+
+def cruzamiento_aleratorio(poblacion_strings:list):
+    """Funcion que realiza el cruzamiento de la poblacion de dos en dos 
+       con un punto de corte aleatorio"""
+    poblacion_cruzada = []
+    n_chromosomes = len(poblacion_strings)
+    for i in range(0, n_chromosomes//2):
+        cromosoma1 = list(poblacion_strings[i])
+        cromosoma2 = list(poblacion_strings[n_chromosomes-i-1])
+        punto_corte = random.randint(1, len(cromosoma1)-2)
+        poblacion_cruzada.append(cromosoma1[punto_corte:]+cromosoma2[:punto_corte])
+        poblacion_cruzada.append(cromosoma2[punto_corte:] + cromosoma1[:punto_corte])
+    return poblacion_cruzada
+
 def main(num_crom, num_gen): 
     poblacion_inicial = []
     cromosoma = []
