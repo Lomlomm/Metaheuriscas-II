9f16d774312e13f7ed7489eea1cd6928f7d12072
Pablo-Naranjo <pabnar1205@gmail.com>
2023-01-29 12:07:08 -0600

    Ejercico  3 con un pequeño detalle

--- algoritmo_genetico_v3.py
+++ algoritmo_genetico_v3.py
@@ -13,15 +13,17 @@ import argparse
 
 #recibe 4 cromosomas madre, padre, hijo1, hijo2
 def elegirMejores(*args): 
-    cromosoma_mejor_1, cromosoma_mejor_2  = [],[]
+    mejores_cromosomas = []
     
     # Ordenamos los cromosomas de entrada de mayor a menor puntaje 
-    ordenado = sorted(args, key=lambda x: x.count('0'), reverse=True)
+    ordenado = sorted(args[0], key=lambda x: x.count('0'), reverse=True)
+    #print(ordenado)
 
-    # Asignamos los primeros 2 cromosomas de la lista ordenada a las variables de mejores cromosomas 
-    cromosoma_mejor_1 = ordenado[0]
-    cromosoma_mejor_2 = ordenado[1]
-    return cromosoma_mejor_1, cromosoma_mejor_2
+    # Asignamos los primeros 6 cromosomas de la lista ordenada a las variables de mejores cromosomas 
+    for i in range(6):
+        mejores_cromosomas.append(ordenado[i])
+    
+    return mejores_cromosomas
 
 def encuentra_optimo(cromosoma1, cromosoma2,  num_gen): 
     #verifica si los cromosomas tienen una cantidad de '0' igual a la longitud de la lista 
@@ -42,26 +44,42 @@ def cruzamiento_aleatorio(poblacion_strings:list):
     """Funcion que realiza el cruzamiento de la poblacion tomando 3 pares de cromosomas aletoriamente
     cruzandolos de dos en dos de manera aleatoria y regresando la nueva poblacion"""
 
-    n_chromosomes = len(poblacion_strings)
+    # Convertimos en lista la población de strings
+    poblacion_strings = list(poblacion_strings)
+
+    nueva_poblacion = []
+
     for i in range(3):
+
+        n_chromosomes = len(poblacion_strings)
+
         # Tomamos los dos cromosomas padres 
-        cromosoma1 = list(poblacion_strings[random.randint(0,n_chromosomes-i)])
-        cromosoma2 = list(poblacion_strings[random.randint(0,n_chromosomes-i-1)])
+        cromosoma1 = poblacion_strings.pop(random.randint(0, n_chromosomes - 1))
+        cromosoma2 = poblacion_strings.pop(random.randint(0, n_chromosomes - 2))
 
         punto_corte = random.randint(1, len(cromosoma1)-2)
         # Generamos los cromosomas hijos a partir del cruzamiento a un punto aleatorio
         cromosoma_hijo_1 = cromosoma1[punto_corte:] + cromosoma2[:punto_corte]
         cromosoma_hijo_2 = cromosoma2[punto_corte:] + cromosoma1[:punto_corte]
 
-        # Evaluamos y tomamos los dos mejores cromosomas de ambos pares
-        cromosoma_optimo_1, cromosoma_optimo_2 = elegirMejores(cromosoma1, cromosoma2, cromosoma_hijo_1, cromosoma_hijo_2)
-        
-        # Acomodamos los cromosomas más óptimos en las posiciones que tenían los padres
-        poblacion_strings[i] = cromosoma_optimo_1
-        poblacion_strings[n_chromosomes-i-1] = cromosoma_optimo_2
+        # Juntamos padres e hijo para evaluarlos
+        nueva_poblacion.append(cromosoma1)
+        nueva_poblacion.append(cromosoma2)
+        nueva_poblacion.append(cromosoma_hijo_1)
+        nueva_poblacion.append(cromosoma_hijo_2)
 
-    #Regresamos la nueva población con los cruzamientos aplicados 
-    return poblacion_strings, cromosoma_optimo_1, cromosoma_optimo_2
+    # Evaluamos y tomamos los dos mejores cromosomas de ambos pares
+    #print(nueva_poblacion)
+    cromosomas_optimos = elegirMejores(tuple(nueva_poblacion))
+    
+    # Acomodamos los cromosomas más óptimos en las primers posiciones
+    for i in range(len(cromosomas_optimos)):
+        poblacion_strings.append(cromosomas_optimos[i])
+
+
+    #Regresamos la nueva población con los cruzamientos aplicados, y los valores 4 y 5
+    # ya que se agregaron al final de la litas 
+    return poblacion_strings, poblacion_strings[4], poblacion_strings[5]
 
      
     
@@ -83,14 +101,18 @@ def main(num_crom, num_gen):
         # Verificamos si el cromosoma de sólo 0 es encontrado 
         # Si lo encontramos, se detiene el while, sino, continua 
         optimo_encontrado = encuentra_optimo(cromosoma_optimo_1, cromosoma_optimo_2, num_gen)
+        if count == 249 or optimo_encontrado == True: 
+            break
 
         # Ordenamos la nueva población creada del previo cruzamiento 
         poblacion = ordenar_poblacion(poblacion_inicial)
         # Cruzamos la población ordenada 
         poblacion, cromosoma_optimo_1, cromosoma_optimo_2 = cruzamiento_aleatorio(poblacion)
-        count += 1 
-        if count == 249 or optimo_encontrado == True: 
-            break 
+        """print("optimos")
+        print(cromosoma_optimo_1)
+        print(cromosoma_optimo_2)
+        print(poblacion)"""
+        count += 1  
         
     print(poblacion)
     print(f"Generacion: {count}")
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+poblacion_strings
